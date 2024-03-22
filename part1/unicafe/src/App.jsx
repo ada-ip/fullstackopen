3212640d6e85b19d25c49cafa7eefb1241e2e0f8
ada-ip <ada.iglesias.pasamontes@gmail.com>
2024-03-22 17:09:23 +0100

    Add 1.8: unicafe step3 solution

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,27 +10,35 @@ const Display = ({ text, value }) => (
 	</p>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+	const calculateAverage = () => {
+		let avg = good * 1 + neutral * 0 + bad * -1;
+		if (avg === 0) return 0;
+		return avg / (good + neutral + bad);
+	};
+
+	const calculatePositivePercentage = () => {
+		if (good + neutral + bad === 0) return 0;
+		return (100 * good) / (good + neutral + bad);
+	};
+
+	return (
+		<section>
+			<Header text="Statistics" />
+			<Display text="Good" value={good} />
+			<Display text="Neutral" value={neutral} />
+			<Display text="Bad" value={bad} />
+			<Display text="All" value={bad + neutral + good} />
+			<Display text="Average" value={calculateAverage()} />
+			<Display text="Positive" value={`${calculatePositivePercentage()}%`} />
+		</section>
+	);
+};
+
 const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
-
-	const calculateAverage = (data) => {
-		const categoriesValues = {
-			good: 1,
-			neutral: 0,
-			bad: -1
-		};
-		let avg = 0;
-		data.forEach((d) => (avg += categoriesValues[d.category] * d.number));
-		if (avg === 0) return 0;
-		return avg / data.reduce((acc, curr) => acc + curr.number, 0);
-	};
-
-	const calculatePercentage = (number, all) => {
-		if (all === 0) return 0;
-		return (100 * number) / all;
-	};
 
 	return (
 		<>
@@ -40,22 +48,7 @@ const App = () => {
 				<Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
 				<Button onClick={() => setBad(bad + 1)} text="Bad" />
 			</section>
-			<section>
-				<Header text="Statistics" />
-				<Display text="Good" value={good} />
-				<Display text="Neutral" value={neutral} />
-				<Display text="Bad" value={bad} />
-				<Display text="All" value={bad + neutral + good} />
-				<Display
-					text="Average"
-					value={calculateAverage([
-						{ category: "good", number: good },
-						{ category: "neutral", number: neutral },
-						{ category: "bad", number: bad }
-					])}
-				/>
-				<Display text="Positive" value={`${calculatePercentage(good, bad + neutral + good)}%`} />
-			</section>
+			<Statistics good={good} neutral={neutral} bad={bad} />
 		</>
 	);
 };
