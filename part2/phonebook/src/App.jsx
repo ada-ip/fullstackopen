617f1a4919f26f6bd36c9f7fee42df7a063695cd
ada-ip <ada.iglesias.pasamontes@gmail.com>
2024-05-03 17:29:49 +0200

    Add 2.8: The Phonebook Step 3 solution

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: "040-123456" }]);
 	const [newName, setNewName] = useState("");
+	const [newPhone, setNewPhone] = useState("");
 
 	const addPerson = (e) => {
 		e.preventDefault();
-		if (!newName) return;
+		if (!newName || !newPhone) {
+			alert(`Both name and phone are mandatory`);
+			return;
+		}
 		if (persons.some((person) => person.name === newName)) {
 			alert(`${newName} is already added to the phonebook`);
 			return;
 		}
-		setPersons([...persons, { name: newName }]);
+		if (persons.some((person) => person.phone === newPhone)) {
+			alert(`${newPhone} is already added to the phonebook`);
+			return;
+		}
+		const newPerson = {
+			name: newName,
+			phone: newPhone
+		};
+		setPersons([...persons, newPerson]);
 		setNewName("");
+		setNewPhone("");
 	};
 
 	return (
@@ -23,13 +36,18 @@ const App = () => {
 					name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
 				</div>
 				<div>
+					number: <input value={newPhone} onChange={(e) => setNewPhone(e.target.value)} />
+				</div>
+				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
 			<ul>
 				{persons.map((person, i) => (
-					<li key={i}>{person.name}</li>
+					<li key={i}>
+						{person.name} {person.phone}
+					</li>
 				))}
 			</ul>
 		</div>
